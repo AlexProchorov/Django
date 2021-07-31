56c82605c3c8f6b3e9e9baeee3129790eaa48099
Alex <proch0r0v@yandex.ru>
2021-07-31 16:23:54 +0300

    2

---
+++ geekShop/mainapp/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, get_object_or_404
+from mainapp.models import Product
+from basketapp.models import Basket
+from mainapp.models import ProductCategory
+
+
+def products(request, pk=None):
+    title = 'продукты'
+
+    links_menu = ProductCategory.objects.all()
+    same_products = Product.objects.all()[:4]
+
+    basket = []
+    if request.user.is_authenticated:
+        basket=Basket.objects.filter(user=request.user)
+
+
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk = pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        context = {
+            'title':title,
+            'links_menu': links_menu,
+            'related_products': same_products,
+            'products': products
+        }
+        return render(request, 'mainapp/products.html', context)
+
+
+
+
+    products = Product.objects.all().order_by('price')
+
+
+
+    context = {
+        'title':title,
+        'links_menu': links_menu,
+        'related_products': same_products,
+
+        'products': products
+
+    }
+
+    return render(request, 'mainapp/products.html', context)
